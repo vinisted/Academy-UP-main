1b47a409c6bc7a0a49ef3e8e32af0dccc096617c
Vinicius Araújo de Figueiredo <54505830+vinisted@users.noreply.github.com>
2023-10-05 18:27:32 -0300

    correção do titulo da pagina home

--- pages/home.tsx
+++ pages/home.tsx
@@ -29,7 +29,7 @@ const HomeAuth = function (){
     return(
         <>
         <Head>
-            <title>Academy UP - Home</title>
+            <title>AcademyUp - Home</title>
             <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
         </Head>
         <main>
